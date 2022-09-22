72f5d1e5fc74821af42766338257adb8db4fe904
isakhalin <47768954+isakhalin@users.noreply.github.com>
2022-09-22 20:28:08 +1100

    Lesson-01. 1:05:00

--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
-4 Четверть. TypeScript
+### 4 Четверть. TypeScript
+#### Lesson 1
+
+   "scripts": {
+     "build": "tsc src/index.ts --outDir dist --target es2015",
+     "test": "echo \"Error: no test specified\" && exit 1"
+  },
---
+++ src/index.ts
@@ -0,0 +1,33 @@
+class Book {
+    name = '';
+    genre = '';
+    pageAmount = 0;
+
+    constructor(name: string, genre: string, pageAmount: number) {
+        this.name = name
+        this.genre = genre
+        this.pageAmount = pageAmount
+    }
+}
+
+const books: Book[] = [
+    new Book('Harry Potter', 'fantasy', 980),
+    new Book('Lord of the Ring', 'fantasy', 1001),
+    new Book('How to be productive', 'lifestyle', 500),
+    new Book('Game of Thrones', 'fantasy', 999)
+]
+
+function findSuitableBook(genre: string, pagesLimit: number, multipleRecommendations = true): Book | Book[] | string {
+    const findAlgorithm = (book: Book) => {
+        return book.genre === genre && book.pageAmount <= pagesLimit
+    }
+
+    if (multipleRecommendations) {
+        return books.filter(findAlgorithm)
+    } else {
+        return books.find(findAlgorithm) ? books.find(findAlgorithm) : 'Книга не найдена'
+    }
+}
+
+console.log(findSuitableBook('fantasy', 1000, false));
+console.log(findSuitableBook('fantasy', 1000));
