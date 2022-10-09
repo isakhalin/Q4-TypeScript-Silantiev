8f97ceaeb9bbe051fb6e19f4e3ca9b5a54b861a3
isakhalin <47768954+isakhalin@users.noreply.github.com>
2022-10-09 12:50:03 +1100

    lesson01

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,44 @@
-class Book {
-    name = '';
-    genre = '';
-    pageAmount = 0;
+interface IBook {
+  name: string,
+  genre: string,
+  pageAmount: number
+}
 
-    constructor(name: string, genre: string, pageAmount: number) {
-        this.name = name
-        this.genre = genre
-        this.pageAmount = pageAmount
-    }
+class Book {
+  name = '';
+  genre = '';
+  pageAmount = 0;
+
+  constructor({name, genre, pageAmount}: IBook) {
+    this.name = name
+    this.genre = genre
+    this.pageAmount = pageAmount
+  }
 }
 
 const books: Book[] = [
-    new Book('Harry Potter', 'fantasy', 980),
-    new Book('Lord of the Ring', 'fantasy', 1001),
-    new Book('How to be productive', 'lifestyle', 500),
-    new Book('Game of Thrones', 'fantasy', 999)
+  new Book(
+    {
+      name: 'Harry Potter',
+      genre: 'fantasy',
+      pageAmount: 980
+    }
+  ),
+  // new Book('Lord of the Ring', 'fantasy', 1001),
+  // new Book('How to be productive', 'lifestyle', 500),
+  // new Book('Game of Thrones', 'fantasy', 999)
 ]
 
 function findSuitableBook(genre: string, pagesLimit: number, multipleRecommendations = true): Book | Book[] | string {
-    const findAlgorithm = (book: Book) => {
-        return book.genre === genre && book.pageAmount <= pagesLimit
-    }
+  const findAlgorithm = (book: IBook) => {
+    return book.genre === genre && book.pageAmount <= pagesLimit
+  }
 
-    if (multipleRecommendations) {
-        return books.filter(findAlgorithm)
-    } else {
-        return books.find(findAlgorithm) ? books.find(findAlgorithm) : 'Книга не найдена'
-    }
+  if (multipleRecommendations) {
+    return books.filter(findAlgorithm)
+  } else {
+    return books.find(findAlgorithm) ? books.find(findAlgorithm) : 'Книга не найдена'
+  }
 }
 
 console.log(findSuitableBook('fantasy', 1000, false));
-console.log(findSuitableBook('fantasy', 1000));
