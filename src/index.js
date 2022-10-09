dafdfb3b74936d6e61239e8e7fef690948ef0299
isakhalin <47768954+isakhalin@users.noreply.github.com>
2022-10-09 12:54:27 +1100

    HW1

--- src/index.js
+++ src/index.js
@@ -1,31 +1,14 @@
-var Book = /** @class */ (function () {
-    function Book(name, genre, pageAmount) {
-        this.name = '';
-        this.genre = '';
-        this.pageAmount = 0;
-        this.name = name;
-        this.genre = genre;
-        this.pageAmount = pageAmount;
-    }
-    return Book;
-}());
-var books = [
-    new Book('Harry Potter', 'fantasy', 980),
-    new Book('Lord of the Ring', 'fantasy', 1001),
-    new Book('How to be productive', 'lifestyle', 500),
-    new Book('Game of Thrones', 'fantasy', 999)
-];
-function findSuitableBook(genre, pagesLimit, multipleRecommendations) {
-    if (multipleRecommendations === void 0) { multipleRecommendations = true; }
-    var findAlgorithm = function (book) {
-        return book.genre === genre && book.pageAmount <= pagesLimit;
-    };
-    if (multipleRecommendations) {
-        return books.filter(findAlgorithm);
-    }
-    else {
-        return books.find(findAlgorithm) ? books.find(findAlgorithm) : 'Книга не найдена';
-    }
-}
-console.log(findSuitableBook('fantasy', 1000, false));
-console.log(findSuitableBook('fantasy', 1000));
+import { renderSearchFormBlock } from './search-form.js'
+import { renderSearchStubBlock } from './search-results.js'
+import { renderUserBlock } from './user.js'
+import { renderToast } from './lib.js'
+
+window.addEventListener('DOMContentLoaded', () => {
+  renderUserBlock('0')
+  renderSearchFormBlock()
+  renderSearchStubBlock()
+  renderToast(
+      {text: 'Это пример уведомления. Используйте его при необходимости', type: 'success'},
+      {name: 'Понял', handler: () => {console.log('Уведомление закрыто')}}
+  )
+})
